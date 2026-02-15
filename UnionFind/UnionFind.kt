6c5b0ae34666a013892b421a2d6791f3c5880c14
nmin11 <nmin1124@gmail.com>
2026-02-16 01:47:26 +0900

    add: Union Find

---
+++ UnionFind/UnionFind.kt
@@ -0,0 +1,18 @@
+class UnionFind(size: Int) {
+    private val parent: IntArray = IntArray(size) { it }
+
+    fun find(x: Int): Int {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+
+    fun union(x: Int, y: Int) {
+        val rootX = find(x)
+        val rootY = find(y)
+        if (rootX != rootY) {
+            parent[rootY] = rootX
+        }
+    }
+}
